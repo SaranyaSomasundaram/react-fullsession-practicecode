9091db4caa8443aeaec68ab3a57d4266537563c6
SaranyaSomasundaram <saranyasomasundaram92@gmail.com>
2022-03-26 08:35:05 +0530

    problem

--- sum/sum.js
+++ sum/sum.js
@@ -21,4 +21,85 @@ function recursion(a, i = 0) {
 }
 //output
 recursion([10, 20, 30, 40])
-[20, 40, 60, 80] 
+[20, 40, 60, 80]
+------------------* --------------------------------
+//find that how many times the item repeated usig object:
+var a = ["apple", "mango", "apple", "jackfruit", "mango", "apple"]
+
+//using forEach
+var result = [];
+a.forEach((item) => {
+    if (result[item]) {
+
+        result[item] = result[item] + 1
+
+    } else {
+
+        result[item] = 1
+    }
+
+})
+//output
+result
+{ apple: 3, mango: 2, jackfruit: 1 }
+
+//using for loop:
+var getresult = {}
+for (var i = 0; i < a.length; i++) {
+    var item = a[i]
+    if (getresult[item]) {
+        getresult[item] = getresult[item] + 1
+
+    } else {
+
+        getresult[item] = 1
+
+    }
+
+
+}
+//output
+getresult
+{ apple: 3, mango: 2, jackfruit: 1 }
+
+//using reducemethod
+
+a.reduce((acc, item) => {
+    if (acc[item]) {
+        acc[item] = acc[item] + 1
+    } else {
+        acc[item] = 1
+
+    }
+
+    return acc;
+
+}, {})
+
+//output
+{ apple: 3, mango: 2, jackfruit: 1 }
+
+//using recursion:
+
+var getdata = {}
+function recursion(a, i = 0) {
+    if (i < a.length) {
+
+        if (getdata[a[i]]) {
+
+            getdata[a[i]] = getdata[a[i]] + 1;
+        } else {
+
+            getdata[a[i]] = 1
+        }
+        i++;
+        recursion(a, i)
+
+    }
+    return getdata
+}
+
+//output:
+recursion(["apple", "mango", "apple", "jackfruit", "mango", "apple"])
+{ apple: 3, mango: 2, jackfruit: 1 }
+
